d7c8e33049b3736f9861d1620f594bcdd227ab52
danielsanchez68 <danielsanchez68@hotmail.com>
2022-03-30 17:28:08 -0300

    v0.2.1

--- tick.js
+++ tick.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 
 setInterval(async () => {
-    console.log((await axios('https://appusdblue.herokuapp.com/')).status)
+    //console.log((await axios('https://appusdblue.herokuapp.com/')).status)
+    console.log((await axios('https://appusdblue.glitch.me/')).status)
 },60000)
