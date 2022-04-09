677f40c16996b596f93fd059b5892b0f1c0090a0
danielsanchez68 <danielsanchez68@hotmail.com>
2022-04-09 14:03:22 -0300

    v0.2.5

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const URL_BASE = process.env.URL_BASE || 'mongodb://localhost:27017/mibase'
-const VERSION = 'v0.2.4'
-const TMS_GETDOLARAPI = 60100/*  5000 */
+const VERSION = 'v0.2.5'
+const TMS_GETDOLARAPI = 60100 /* 5000 */
 
 export default {
     URL_BASE,
--- public/js/main.js
+++ public/js/main.js
@@ -11,33 +11,67 @@ async function iniRepreData() {
     }
 }
 
+
+let respuesta
+let startTimestampAnt = true
+let endTimestampAnt = true
+let contPedido = 0
+
 async function tickRepreData() {
     //tomo la hora del input y muestro el modo de operación
     const startTime = document.getElementById('start-time').value
-    const startTimestamp = new Date(startTime).getTime()
+    const startTimestamp = new Date(startTime).getTime() || 0
 
     const endTime = document.getElementById('end-time').value
-    const endTimestamp = new Date(endTime).getTime()
+    const endTimestamp = new Date(endTime).getTime() || 0
 
-    //tomo el step de minutos
-    const stepMin = document.getElementById('step-min').value
-
-    document.getElementById('modo').innerText = startTimestamp && endTimestamp ?
-        `Mostrando desde fecha inicial: ${new Date(startTime).toLocaleString()} hasta fecha final: ${new Date(endTime).toLocaleString()}` :
-        `Mostrando última hora`
-
-    document.getElementById('modo').innerText += ` en pasos de ${stepMin?stepMin: 1} minuto(s)`
+    contPedido++
+    //console.log(contPedido)
+    /* console.log(startTimestamp, startTimestampAnt )
+    console.log(endTimestamp, endTimestampAnt )
+    console.log(startTimestamp != startTimestampAnt )
+    console.log(endTimestamp != endTimestampAnt ) */
 
     //petición de los datos al backend
-    const { data: respuesta } = await axios(`/data/${startTimestamp}/${endTimestamp}/`)
+    if( 
+        (!startTimestamp && !endTimestamp && (contPedido>=60) ) ||
+        (startTimestamp != startTimestampAnt) ||
+        (endTimestamp != endTimestampAnt) 
+    ) {
+        console.log('Pidiendo...', Date.now())
+        const { data: rta } = await axios(`/data/${startTimestamp}/${endTimestamp}/`)
+        respuesta = rta
 
-    const datos = respuesta.datos
-    const valorVentaActual = datos[datos.length-1]?.dolar || '?'
-    const tsvalorVentaActual = datos[datos.length-1]?.timestamp || '?'
-    document.getElementById('valor-venta').innerHTML = 
-        `$${valorVentaActual } <i>(${new Date(tsvalorVentaActual).toLocaleString()})</i>`
+        startTimestampAnt = startTimestamp
+        endTimestampAnt = endTimestamp
 
-    graf(datos, stepMin && stepMin!=0? stepMin : 1) 
+        contPedido = 0
+    }
+
+    if(respuesta) {
+        const datos = respuesta.datos
+        const valorVentaActual = datos[datos.length-1]?.dolar || '?'
+        const tsvalorVentaActual = datos[datos.length-1]?.timestamp || '?'
+
+        //tomo el step de minutos
+        let stepMin = document.getElementById('step-min').value
+        
+        if(datos.length >= 1000 && datos.length < 5000 && stepMin < 5) stepMin = 5
+        if(datos.length >= 5000 && datos.length < 10000 && stepMin < 10) stepMin = 10
+        if(datos.length >= 10000 && stepMin < 20) stepMin = 20
+        
+        document.getElementById('valor-venta').innerHTML = 
+            `$${valorVentaActual } <i>(${new Date(tsvalorVentaActual).toLocaleString()})</i>`
+
+        document.getElementById('modo').innerText = startTimestamp && endTimestamp ?
+            `Mostrando desde fecha inicial: ${new Date(startTime).toLocaleString()} hasta fecha final: ${new Date(endTime).toLocaleString()}` :
+            `Mostrando última hora`
+    
+        document.getElementById('modo').innerText += ` en pasos de ${stepMin?stepMin: 1} minuto(s)`
+    
+    
+        graf(datos, stepMin && stepMin!=0? stepMin : 1) 
+    }
 
     //represento la hora actual
     document.getElementById('fyh').innerText = new Date().toLocaleString()
@@ -66,64 +100,64 @@ function timestamp2fyh(dato) {
 }
 
 const graf = (datosin,step) => {
+    //console.log(datosin)
 
-    const datos = datosin.filter((dato,index) => index % step == 0)
-    datos.push(datosin[datosin.length-1])
+    if(datosin.length && (typeof datosin[0] != 'undefined')) { 
 
-    if(datos.length) { 
+        const datos = datosin.filter((dato,index) => index % step == 0)
+        datos.push(datosin[datosin.length-1])
+    
         document.getElementById('msg-error').innerHTML = ''
         document.getElementById('myDiv').style.display = 'block'
+
+
+        const fyh = datos.map(dato => timestamp2fyh(new Date(dato.timestamp).toLocaleString()))
+        const dolars = datos.map(dato => dato.dolar)
+
+        var trace = {
+            x: fyh,
+            y: dolars,
+            mode: 'lines+markers'
+            //type: 'scatter'
+        };
+    
+        var data = [trace];
+    
+        var layout = {
+            title: '<b>Dolar histórico</b>',
+            xaxis: {
+                showline: true,
+                showgrid: true,
+                showticklabels: true,
+                linecolor: 'rgb(204,204,204)',
+                linewidth: 2,
+                autotick: false,
+                ticks: 'outside',
+                tickcolor: 'rgb(204,204,204)',
+                tickwidth: 2,
+                ticklen: 5,
+                tickfont: {
+                    family: 'Arial',
+                    size: 10,
+                    color: 'rgb(82, 82, 82)'
+                }
+            },
+            yaxis: {
+                showgrid: true,
+                zeroline: true,
+                showline: true,
+                showticklabels: true,
+                linecolor: 'rgb(204,204,204)',
+            },
+        };
+    
+        Plotly.newPlot('myDiv', data, layout);
+    
     }
     else {
         document.getElementById('myDiv').style.display = 'none'
         document.getElementById('msg-error').innerHTML = '<h2>No se encontraron datos</h2>'
     }
-
-
-
-    const fyh = datos.map(dato => timestamp2fyh(new Date(dato.timestamp).toLocaleString()))
-    const dolars = datos.map(dato => dato.dolar)
-
-
-
-    var trace = {
-        x: fyh,
-        y: dolars,
-        mode: 'lines+markers'
-        //type: 'scatter'
-    };
-
-    var data = [trace];
-
-    var layout = {
-        title: '<b>Dolar histórico</b>',
-        xaxis: {
-            showline: true,
-            showgrid: true,
-            showticklabels: true,
-            linecolor: 'rgb(204,204,204)',
-            linewidth: 2,
-            autotick: false,
-            ticks: 'outside',
-            tickcolor: 'rgb(204,204,204)',
-            tickwidth: 2,
-            ticklen: 5,
-            tickfont: {
-                family: 'Arial',
-                size: 10,
-                color: 'rgb(82, 82, 82)'
-            }
-        },
-        yaxis: {
-            showgrid: true,
-            zeroline: true,
-            showline: true,
-            showticklabels: true,
-            linecolor: 'rgb(204,204,204)',
-        },
-    };
-
-    Plotly.newPlot('myDiv', data, layout);
 }
 
 
