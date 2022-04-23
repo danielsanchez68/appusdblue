78e215579d67cbdcbdcc9a1922785a787217d477
danielsanchez68 <danielsanchez68@hotmail.com>
2022-04-23 20:53:54 -0300

    v0.3.6

--- tick.js
+++ tick.js
@@ -4,7 +4,7 @@ setInterval(async () => {
     try {
         //console.log((await axios('https://appusdblue.herokuapp.com/')).status)
         console.log('Tick alive...')
-        console.log((await axios('https://appusdblue.glitch.me/')).status)
+        console.log((await axios('https://appusdblue2.glitch.me/')).status)
     }
     catch(error) {
         console.log(error.message)
